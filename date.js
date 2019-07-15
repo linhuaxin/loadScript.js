9f6f2be24fd4184980f511ffcb1d436ea7ebfb1e
linhuaxin <linhuaxin@wxchina.com>
2019-07-15 09:39:37 +0800

    date

---
+++ date.js
@@ -0,0 +1,20 @@
+const formatDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+const formatTime = date => {
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [hour, minute, second].map(formatNumber).join(':')
+}
+
+export default {
+  formatTime,
+  formatDate
+}
